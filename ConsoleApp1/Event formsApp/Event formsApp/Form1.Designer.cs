adb9edca5c09a1cdb8d3b01b7bbd565b27165acd
騏瑋 劉 <xxxx6712@gmail.com>
2018-11-18 22:32:29 +0800

    19 chapter create  winform

---
+++ ConsoleApp1/Event formsApp/Event formsApp/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Event_formsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myTextBox = new System.Windows.Forms.TextBox();
+            this.myButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // myTextBox
+            // 
+            this.myTextBox.Location = new System.Drawing.Point(12, 12);
+            this.myTextBox.Name = "myTextBox";
+            this.myTextBox.Size = new System.Drawing.Size(320, 22);
+            this.myTextBox.TabIndex = 0;
+            // 
+            // myButton
+            // 
+            this.myButton.Location = new System.Drawing.Point(12, 40);
+            this.myButton.Name = "myButton";
+            this.myButton.Size = new System.Drawing.Size(320, 23);
+            this.myButton.TabIndex = 1;
+            this.myButton.Text = "SayHello";
+            this.myButton.UseVisualStyleBackColor = true;
+            this.myButton.Click += new System.EventHandler(this.MyButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 363);
+            this.Controls.Add(this.myButton);
+            this.Controls.Add(this.myTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox myTextBox;
+        private System.Windows.Forms.Button myButton;
+    }
+}
+
